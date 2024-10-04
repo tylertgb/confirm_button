c35240b540864474e9c379c9035f014f97259f9f
Tyler Gobka Bright <tgbright88@gmail.com>
2024-10-04 17:25:51 +0000

    updates

--- script.js
+++ script.js
@@ -1,32 +1,64 @@
+const confirmSection = document.querySelector('.row');
 
-function success() {
-const check = document.getElementById('check');
-const btn = document.getElementById('btn');
-const message = document.getElementById('message');
+function submitAnother(){
+    const submitAnother = document.querySelector('.submitAnother');
+    submitAnother.addEventListener('click', (e) => {
+        e.preventDefault();
+        confirmSection.style.display = "none";
+        btn.classList.toggle('hide')
 
-btn.addEventListener('click', () => {
-    btn.classList.add('hide');
-    check.classList.add('rotateIn');
-    message.classList.add('fadeIn');
-});
+        submitAnother.style.display = "none";
+        formStatus.style.opacity = 1;
+    });
 }
-success();
 
-form.addEventListener('submit', function(event) {
+
+function confirmNote(){
+    const check = document.getElementById('check');
+    const btn = document.getElementById('btn');
+    const message = document.getElementById('message');
+    const form = document.getElementById('form');
+
+    btn.addEventListener('click', (e) => {
     // Prevent the default form submission action
-    event.preventDefault();
+    e.preventDefault();
 
     // Get form data values
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    const age = document.getElementById('age').value;
+    const password = document.getElementById('password').value;
 
-    // Check if any of the input fields are empty
-    if (name === '' || email === '' || age === '') {
-        formStatus.innerHTML = "Please fill in all the fields!";
-        formStatus.style.color = "red";
-    } else {
-        formStatus.innerHTML = "Form submitted successfully!";
-        formStatus.style.color = "green";
+    function confirmed(){
+        confirmSection.style.display = "flex";
+        message.innerHTML = "Successfull!";
+        btn.classList.add('hide');
+        check.classList.add('rotateIn');
+        message.classList.add('fadeIn');
     }
-});
+
+    //Clear input data field
+    function clearField(){
+        form.addEventListener('click', (e) => {
+            e.preventDefault();
+            form.reset();
+        })
+    }
+
+        // Check if any of the input fields are empty
+    if (name === '' || email === '' || password === '') {
+        document.getElementById('formStatus').innerHTML = "Please fill in all the fields!";
+        formStatus.style.color = "red";
+        } else {
+            formStatus.style.opacity = 0;
+
+            // Show confirmation button and checkmark section
+            confirmed();
+            clearField();
+
+            document.querySelector('.submitAnother').style.display = "flex";
+        }
+    });
+
+}
+confirmNote();
+submitAnother();
